e2fb7415725e92bf1f014f1f108be8b9ff5a9897
madden20202 <business.mocksock@gmail.com>
2022-07-18 10:20:21 -0500

    add sample code with notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,25 @@
+// Note, do not compile code
+
+/*
+  This is a basic for loop that would have an output 
+  Hello Number 0
+  Hello Number 1
+  Hello Number 2
+  Hello Number 3
+  Hello Number 4
+  Hello Number 5
+
+  This is all coming from the void main function, which 
+  is special because it is the entry point of the app,
+  so no other method should use main
+
+  Unlike other languages, Dart uses ; to separate the 
+  different expressions
+  Like other languages though, it is Typed and uses 
+  lower camel case for its methods
+  EX: makeUserLaugh()
+*/
+
 void main() {
   for (int i = 0; i <= 5; i++) {
     print("Hello Number $i");
