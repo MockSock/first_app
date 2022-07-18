3aa345263d78e2d70d4cc2a8c8228e5d3f86aab5
madden20202 <business.mocksock@gmail.com>
2022-07-18 14:19:26 -0500

    add method to main and change the for loop

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 
 /*
   This is a basic for loop that would have an output 
-  Hello Number 0
   Hello Number 1
   Hello Number 2
   Hello Number 3
@@ -11,17 +10,27 @@
 
   This is all coming from the void main function, which 
   is special because it is the entry point of the app,
-  so no other method should use main
+  so no other method should use main. But within this,
+  we can call other functions, or even entire blocks of code
+
+  This creates the Widget Tree, where everything branches from main(),
+  and is made into States
 
   Unlike other languages, Dart uses ; to separate the 
   different expressions
   Like other languages though, it is Typed and uses 
-  lower camel case for its methods
-  EX: makeUserLaugh()
+  lower camel case for its methods, whcih can take parameters
+  Let's take user input as an example
 */
 
 void main() {
-  for (int i = 0; i <= 5; i++) {
+  displayNumbersToText();
+}
+
+displayNumbersToText(int i) {
+  //Change so that the input from the method will
+  // then be in the for loop
+  for (i; i <= i * 2; i++) {
     print("Hello Number $i");
   }
 }
