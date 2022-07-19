68ffc6a6890533055bbbe98f4649b7704c3ac16e
madden20202 <business.mocksock@gmail.com>
2022-07-19 13:13:07 -0500

    create first widget and add notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
 void main() {}
+
+// Now since everything is a widget, we need to create it
+// Widgets are a special type of class, which come in two flavors
+
+// Stateless, where nothing changes
+// EX: A logo or a static loading screen
+
+// Stateful, where change occurs, may it be by the app or user
+// EX: A new user form that will be sent to the app or an animation
+
+class MyApp extends StatelessWidget {}
