aa88467dd5e9a84e87ba6ae7357d0422d19de900
madden20202 <business.mocksock@gmail.com>
2022-07-20 13:15:39 -0500

    Add button to widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: null,
+        child: Text('Answer 1'),
+      ),
+    );
   }
 }
