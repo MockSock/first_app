3c166cd1f6de82d84e6046715a88af39469227de
madden20202 <business.mocksock@gmail.com>
2022-07-20 08:27:44 -0500

    add basic framework for question widget

--- lib/question.dart
+++ lib/question.dart
@@ -1,3 +1,10 @@
-// General rule of thumb is one widget 
-// per file, so code is more 
+// General rule of thumb is one widget
+// per file, so code is more
 // reusable and keeps files clean
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
