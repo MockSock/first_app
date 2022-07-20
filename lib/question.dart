36de421f157e444c429a79ad9fe54bd0994c49ea
madden20202 <business.mocksock@gmail.com>
2022-07-20 11:59:02 -0500

    perform basic restyling

--- lib/question.dart
+++ lib/question.dart
@@ -13,6 +13,12 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
