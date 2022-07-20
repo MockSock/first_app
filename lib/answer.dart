21659b691d52ea4107461d420723d3e284cc4c7e
madden20202 <business.mocksock@gmail.com>
2022-07-20 13:10:25 -0500

    rename widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Buttons extends StatelessWidget {
+class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
