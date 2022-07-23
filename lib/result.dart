de0532668a0255ddce30ba11a203ce6bf2ddbba4
madden20202 <business.mocksock@gmail.com>
2022-07-22 21:31:07 -0500

    return a variable withing a getter and display it

--- lib/result.dart
+++ lib/result.dart
@@ -4,10 +4,24 @@ class Result extends StatelessWidget {
   final int resultScore;
 
   Result(this.resultScore);
+
+  // Let's make a getter, which is a mix
+  // between a method and a property
+  String get resultPhrase {
+    var resultText = 'You did it!';
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
